f0cecd7de6f6c07893d8149370d4ef75a024fdf0
S Pramila <105471405+Pramila-S@users.noreply.github.com>
2022-07-27 14:20:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+setTimeout(() => {
+    let a=document.getElementById('test').innerHTML=10;
+    console.log(a);
+    setTimeout(() => {
+        let b=document.getElementById('test').innerHTML=9;
+        console.log(b);
+        setTimeout(() => {
+            let c=document.getElementById('test').innerHTML=8;
+            console.log(c);
+            setTimeout(() => {
+                let d=document.getElementById('test').innerHTML=7;
+                console.log(d);
+                setTimeout(() => {
+                    let e=document.getElementById('test').innerHTML=6;
+                    console.log(e);
+                    setTimeout(() => {
+                        let f=document.getElementById('test').innerHTML=5;
+                          console.log(f);
+                        setTimeout(() => {
+                            let g=document.getElementById('test').innerHTML=4;
+                            console.log(g);
+                            setTimeout(() => {
+                                let h=document.getElementById('test').innerHTML=3;
+                                console.log(h);
+                                setTimeout(() => {
+                                    let i=document.getElementById('test').innerHTML=2;
+                                    console.log(i);
+                                    setTimeout(() => {
+                                        let j=document.getElementById('test').innerHTML=1;
+                                        console.log(j);
+                                        setTimeout(() =>{
+                                            let k = document.getElementById('test').innerHTML="Happy Independence Day";
+                                            console.log(k);
+                                        },1000)
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+},1000)
+
